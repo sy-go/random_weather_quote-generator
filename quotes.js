117632ebad1369ebf91fce9dd696af9923aa7b68
Sylwester Gorzelanczyk <sylvek72@gmail.com>
2023-11-25 20:22:48 +0000

    first commit on quotes.js

---
+++ quotes.js
@@ -0,0 +1,17 @@
+function generateRandNum(num){
+return Math.floor(Math.random() * num);
+}
+
+const wordsBank = {
+    words1 :['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Satudray', 'Sunday'],
+    words2 :['Sunny','Partly Sunny','Cloudy', 'Rainy', 'Drizzle','Patchy Rain'],
+    words3 : ['Low', 'Low-Moderate', 'Moderate','Strong','Storm', 'Hurricane'] 
+}
+
+let wordIndex = generateRandNum(wordsBank.words1.length)
+//console.log(wordsBank.words1[wordIndex])
+
+function weatherForecast(){
+    return `On ${wordsBank.words1[wordIndex]}, we expect to have ${wordsBank.words2[wordIndex]} weather, with some ${wordsBank.words3[wordIndex]} wind.`
+}
+console.log(weatherForecast());
